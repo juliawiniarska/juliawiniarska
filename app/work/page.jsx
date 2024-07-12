ad1e07e89ee0962604ae6e2ec8a53b715d83fb60
Julia <julciagim@gmail.com>
2024-07-12 17:58:40 +0200

    changes

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,60 +1,56 @@
 "use client";
 
-import { motion } from 'framer-motion'; 
+import { motion } from 'framer-motion';
 import React, { useState } from 'react';
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-
-import { BsArrowUpRight, BsGithub } from "react-icons/bs"; 
-
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
-
 import Link from "next/link";
 import Image from "next/image";
 import WorkSliderBtns from "@/components/WorkSliderBtns";
 
 const projects = [
   {
-  num: "01",
-  category: "fullstack",
-  title: "Online Store",
-  description:
-  "This project is an MERN stack online store with Redux Toolkit. It involves server setup, MongoDB integration, and UI design featuring a shopping cart, product reviews, carousel, pagination, search, user profiles, and admin management.",
-  stack: [
-    {
-      category: "Front-end",
-      technologies: [
-        { name: "React" },
-        { name: "Redux" }
-      ]
-    },
-    {
-      category: "Back-end",
-      technologies: [
-        { name: "Express" },
-        { name: "JWT Authentication"}
-      ]
-    },
-    {
-      category: "Database",
-      technologies: [
-        { name: "MongoDB" }
-      ]
-    }
-  ],
-  image: "/assets/webproject.png",
-  live: "",
-  github: "https://github.com/juliawiniarska/shop-project",
+    num: "01",
+    category: "fullstack",
+    title: "Online Store",
+    description:
+      "This project is an MERN stack online store with Redux Toolkit. It involves server setup, MongoDB integration, and UI design featuring a shopping cart, product reviews, carousel, pagination, search, user profiles, and admin management.",
+    stack: [
+      {
+        category: "Front-end",
+        technologies: [
+          { name: "React" },
+          { name: "Redux" }
+        ]
+      },
+      {
+        category: "Back-end",
+        technologies: [
+          { name: "Express" },
+          { name: "JWT Authentication"}
+        ]
+      },
+      {
+        category: "Database",
+        technologies: [
+          { name: "MongoDB" }
+        ]
+      }
+    ],
+    image: "/assets/webproject.png",
+    live: "",
+    github: "https://github.com/juliawiniarska/shop-project",
   },
   {
     num: "02",
     category: "fullstack",
     title: "Educational Quiz Management Application",
     description:
-    "An educational quiz management application developed using Laravel and Vue.js. It allows teachers to create and manage classes, tests, questions, and users, as well as view student results. Students can take assigned tests, check their results, and review their test history.",
+      "An educational quiz management application developed using Laravel and Vue.js. It allows teachers to create and manage classes, tests, questions, and users, as well as view student results. Students can take assigned tests, check their results, and review their test history.",
     stack: [
-      { 
+      {
         category: "Front-end",
         technologies: [
           { name: "Vue.js" },
@@ -62,20 +58,20 @@ const projects = [
           { name: "Bootstrap" }
         ]
       },
-      { 
+      {
         category: "Back-end",
         technologies: [
           { name: "Laravel" },
           { name: "PHP" }
         ]
       },
-      { 
+      {
         category: "Database",
         technologies: [
           { name: "MySQL" }
         ]
       },
-      { 
+      {
         category: "Authentication",
         technologies: [
           { name: "Laravel Jetstream" }
@@ -85,7 +81,7 @@ const projects = [
     image: "/assets/student_home.png",
     live: "",
     github: "https://github.com/juliawiniarska/quiz-web",
-    },
+  },
 ];
 
 const Work = () => {
@@ -98,12 +94,12 @@ const Work = () => {
 
   return (
     <motion.section
-    initial={{opacity: 0}}
-    animate={{
-      opacity: 1,
-      transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
-    }}
-    classname="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+      initial={{opacity: 0}}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
@@ -116,30 +112,35 @@ const Work = () => {
                 {project.category} project
               </h2>
               <p className="text-white/60">{project.description}</p>
-              <ul className="flex gap-4">
-                {project.stack.map((item, index) => {
-                  return (
-                    <li key={index} className="text-xl text-accent">
-                      {item.name}
-                      {index !== project.stack.length - 1 && ","}
-                    </li>
-                  );
-                })}
+              <ul className="flex gap-4 flex-wrap">
+                {project.stack.map((item, index) => (
+                  <li key={index} className="text-xl text-accent">
+                    <span className="font-bold">{item.category}:</span>{" "}
+                    {item.technologies.map((tech, techIndex) => (
+                      <span key={techIndex}>
+                        {tech.name}
+                        {techIndex !== item.technologies.length - 1 && ", "}
+                      </span>
+                    ))}
+                  </li>
+                ))}
               </ul>
-              <div className="border border-white/20"></div>
+              <div className="border border-white/20 my-4"></div>
               <div className="flex items-center gap-4">
-                <Link href={project.live}>
-                  <TooltipProvider delayDuration={100}>
-                    <Tooltip>
-                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
-                      </TooltipTrigger>
-                      <TooltipContent>
-                        <p>Live project</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </Link>
+                {project.live && (
+                  <Link href={project.live}>
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                          <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Live project</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Link>
+                )}
                 <Link href={project.github}>
                   <TooltipProvider delayDuration={100}>
                     <Tooltip>
@@ -162,23 +163,21 @@ const Work = () => {
               className="xl:h-[520px] mb-12"
               onSlideChange={handleSlideChange}
             >
-              {projects.map((project, index) => {
-                return (
-                  <SwiperSlide key={index} className="w-full">
-                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
-                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
-                      <div className="relative w-full h-full">
-                        <Image
-                          src={project.image}
-                          fill
-                          className="object-cover"
-                          alt=""
-                        />
-                      </div>
+              {projects.map((project, index) => (
+                <SwiperSlide key={index} className="w-full">
+                  <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                    <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                    <div className="relative w-full h-full">
+                      <Image
+                        src={project.image}
+                        fill
+                        className="object-cover"
+                        alt=""
+                      />
                     </div>
-                  </SwiperSlide>
-                );
-              })}
+                  </div>
+                </SwiperSlide>
+              ))}
               <WorkSliderBtns
                 containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
                 btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
