a2e36bfc0069ec1f3f2f744be4949941f6006feb
Julia <julciagim@gmail.com>
2024-07-12 23:43:34 +0200

    changes

--- components/StairsTransition.jsx
+++ components/StairsTransition.jsx
@@ -1,32 +1,33 @@
-"use client"
+"use client";
 
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
+// components
 import Stairs from "./Stairs";
 
-const StairsTransition = () => {
-    const pathname = usePathname();
+const StairTransition = () => {
+  const pathname = usePathname();
   return (
     <>
-    <AnimatePresence mode="wait">
+      <AnimatePresence mode="wait">
         <div key={pathname}>
-            <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none x-40 flex">
-                <Stairs />
-            </div>
+          <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex">
+            <Stairs />
+          </div>
 
-        <motion.div
-        className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
-        initial={{ opacity: 1 }}
-        animate={{
-            opacity: 0,
-            transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
-        }}
-        />
+          <motion.div
+            className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
+            inital={{ opacity: 1 }}
+            animate={{
+              opacity: 0,
+              transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
+            }}
+          />
         </div>
-    </AnimatePresence>
+      </AnimatePresence>
     </>
   );
 };
 
-export default StairsTransition;
+export default StairTransition;
