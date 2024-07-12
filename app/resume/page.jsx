ec1c4997a5c1d75beb6103b9bf69bb357ed1ecdc
Julia <julciagim@gmail.com>
2024-07-13 01:13:15 +0200

    changes

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -200,7 +200,7 @@ const Resume = () => {
                       return (
                         <li
                           key={index}
-                          className="bg-[#232329] h-[250px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                          className="bg-[#232329] max-h-[330px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
                         >
                           <span className="text-accent">{item.duration}</span>
                           <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
