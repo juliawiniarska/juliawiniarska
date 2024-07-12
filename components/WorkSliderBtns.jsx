f4044e4be99934ef4d8cae0d7771d111fa7da233
Julia <julciagim@gmail.com>
2024-07-12 23:45:52 +0200

    changes

--- components/WorkSliderBtns.jsx
+++ components/WorkSliderBtns.jsx
@@ -1,19 +1,19 @@
-"use client"
+"use client";
 
 import { useSwiper } from "swiper/react";
 import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
 
-const WorkSliderBtns = ({ containerStyles, btnStyles, iconStyles }) => {
-    const swiper = useSwiper();
+const WorkSliderBtns = ({ containerStyles, btnStyles, iconsStyles }) => {
+  const swiper = useSwiper();
   return (
     <div className={containerStyles}>
-        <button className={btnStyles} onClick={() => swiper.slidePrev()}>
-            <PiCaretLeftBold className={iconStyles} />
-        </button>
-        <button className={btnStyles} onClick={() => swiper.slideNext()}>
-            <PiCaretRightBold className={iconStyles} />
-        </button>
-        </div>
+      <button className={btnStyles} onClick={() => swiper.slidePrev()}>
+        <PiCaretLeftBold className={iconsStyles} />
+      </button>
+      <button className={btnStyles} onClick={() => swiper.slideNext()}>
+        <PiCaretRightBold className={iconsStyles} />
+      </button>
+    </div>
   );
 };
 
